ac4e351ba33c0ad298e8a535c425011f086c669a
Sara Jourdan <sara.jourdanoff@icloud.com>
2025-05-06 11:39:02 +0200

    feat: add comments

--- Semaine-01/jour-2/pratique-jour-2.js
+++ Semaine-01/jour-2/pratique-jour-2.js
@@ -1,7 +1,7 @@
 "use strict"
 
 // 1 - LES VARIABLES JS
-// Exo1
+/*// Exo1
 let admin;
 let namee = "Jhon";
 admin = namee;
@@ -11,4 +11,13 @@ alert("admin");
 // Exo2
 let notrePlanete = "Terre";
 let currentUserName = "John";
+*/
 
+// 2 - LES TYPES DE DONNÉES
+/* Exo 
+let name = "Ilya"
+
+alert(`hello ${1})
+alert(`hello ${"name"})
+alert(`hello ${name})
+*/
