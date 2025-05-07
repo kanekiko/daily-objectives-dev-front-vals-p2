064b6ffb0e6d1dbb0a8537c7f4cb7ab66f7036d4
Sara Jourdan <sara.jourdanoff@icloud.com>
2025-05-07 13:37:11 +0200

    feat: exo1 comments

--- Semaine-02/apprentissage-gemini-j5.js
+++ Semaine-02/apprentissage-gemini-j5.js
@@ -5,9 +5,9 @@
  * Affiche ensuite l'âge qu'il a entré dans une boîte de dialogue alert().
  * Bonus : Essaie de convertir l'âge entré en nombre et affiche l'âge qu'il aura l'année prochaine dans une autre alert().
  */
-let ageUtilisateurStr = prompt("Renseigner votre âge ci-dessous.", "25");
-alert(ageUtilisateurStr);
+// let ageUtilisateurStr = prompt("Renseigner votre âge ci-dessous.", "25");
+// alert(ageUtilisateurStr);
 
-let ageUtilisateur = Number(ageUtilisateurStr);
-let ageUtilisateur1 = ageUtilisateur + 1;
-alert(ageUtilisateur1);
+// let ageUtilisateur = Number(ageUtilisateurStr);
+// let ageUtilisateur1 = ageUtilisateur + 1;
+// alert(ageUtilisateur1);
